537e93ac25d0e6bb9dd5bc941c7ac4cf66fb116d
Elokvent <38071987+Elokvent@users.noreply.github.com>
2018-04-16 17:46:07 +0200

    update Hello
    New uppdate

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello World");
     System.out.println("hello there");
+    System.out.println("General Kenobi");
   }
 }
